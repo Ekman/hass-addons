9d39a852929c859242c70d47dbcf5ae9e5b13c87
Niklas Ekman <niklas@stockfiller.com>
2020-05-08 10:54:16 +0200

    increase log level

--- valkyrie/docker-entrypoint.sh
+++ valkyrie/docker-entrypoint.sh
@@ -8,4 +8,4 @@ echo "${CRON_PATTERN} node ${APP_PATH} ${CONFIG_PATH}" | crontab -u ${APP_USER} 
 # See: https://stackoverflow.com/questions/37458287/how-to-run-a-cron-job-inside-a-docker-container
 # Start crond with log level 6 in foreground, output to stderr.
 # I found the log level by trial and error.
-crond -f -d 6
+crond -f -d 8
